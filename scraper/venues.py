4e7e7389c203792d87a43350b3affe7c50a6206f
kaijunwong01-stack <kaijun.wong01@gmail.com>
2026-07-09 14:09:07 +0900

    feat: extend scraper to cover 5 Loft venues

--- scraper/venues.py
+++ scraper/venues.py
@@ -2,4 +2,8 @@
 # Adding a new Loft venue later is just adding one more line here.
 VENUES = [
     {"slug": "shelter", "name": "Shelter", "city": "Tokyo"},
+    {"slug": "loft", "name": "Shinjuku Loft", "city": "Tokyo"},
+    {"slug": "loft9", "name": "Loft9 Shibuya", "city": "Tokyo"},
+    {"slug": "lofta", "name": "Asagaya Loft A", "city": "Tokyo"},
+    {"slug": "naked", "name": "Naked Loft", "city": "Yokohama"},
 ]
